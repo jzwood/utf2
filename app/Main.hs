4eadc30e1a22fbd854e3e129e964c1e2dbe57276
Jake Wood <jzwood14@yahoo.com>
2025-02-20 13:32:36 -0500

    reads args right

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,24 @@
+{-# LANGUAGE MultilineStrings #-}
+
 module Main where
 
+import System.Environment (getArgs)
 import qualified Data.ByteString as B
 import Encode (encode)
 
+usage :: String
+usage =
+    """
+    Usage: utf2 (--encode | --decode) <fpath>
+    Options:
+      --encode     converts utf8 data to utf2
+      --decode     converts utf2 data to utf8
+    """
+
 main :: IO ()
-main = B.putStr $ encode "J"
-
-
---import System.Environment (getArgs)
---main :: IO ()
---main = do
-    --args <- getArgs
-    --case args of
-        --[filePath] -> do
-            --content <- readFile filePath
-            ---- Process the content
-        --_ -> putStrLn "Usage: utf2 [--encode | --decode] <filePath>"
+main = do
+    args <- getArgs
+    case args of
+        ["--encode", path] -> B.putStr $ encode path
+        ["--decode", path]  -> B.putStr $ encode path
+        _ -> putStrLn usage
